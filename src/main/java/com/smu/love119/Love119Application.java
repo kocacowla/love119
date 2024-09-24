389b9808393ecf8c85245b4adb4cb75e18ef7217
[Cloud3] 조한휘 <chh498@naver.com>
2024-09-24 19:42:17 +0900

    Initial Commit for love119

---
+++ src/main/java/com/smu/love119/Love119Application.java
@@ -0,0 +1,13 @@
+package com.smu.love119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Love119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Love119Application.class, args);
+	}
+
+}
