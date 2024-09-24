0ef756ac058ad781986055d8f4b590cd66b3ee37
[Cloud3] 조한휘 <chh498@naver.com>
2024-09-24 19:42:17 +0900

    Initial Commit for love119

---
+++ src/test/java/com/smu/love119/Love119ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.love119;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Love119ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
